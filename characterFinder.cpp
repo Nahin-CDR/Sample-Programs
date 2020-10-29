876e9a93334ade40f46b7834addff11c6e360685
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2020-10-29 19:44:12 +0600

    Create characterFinder.cpp

---
+++ characterFinder.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+ 
+int main()
+{
+   
+    
+    
+	string word = "qwertyuiopasdfghjkl;zxcvbnm,./"; 
+	cout << word.find('n');
+
+
+	
+
+    
+    return 0;
+}
