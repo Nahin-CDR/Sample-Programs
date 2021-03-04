8d442e1ec736e1d91cf7e4d497a7b319c59db715
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-03-04 20:48:23 +0600

    Create converting Char to integer.cpp

---
+++ converting Char to integer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//Converting char to integer 
+ 
+int main()
+{
+
+    char a = '1';
+    
+    int b = a - '0';
+    cout<<b+1;
+    
+    
+	return 0;
+}
