75c1efa5b87c7b7c59c11daa49a2f1506ca6fbbd
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-06-25 10:52:17 +0600

    Create convert_String_to_integer.cpp

---
+++ convert_String_to_integer.cpp
@@ -0,0 +1,22 @@
+//Nahin the Coder
+//Date : 25 - 06 - 2021
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	//Converting string to int
+	
+	string str = "12345" ;
+	stringstream ss(str);
+	
+	
+	int x = 0;
+	ss>>x;
+	cout<<"result in integer form : "<<x;
+	
+	
+	
+	
+	return 0;
+}
