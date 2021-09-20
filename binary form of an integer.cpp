9d849a3b59363de028379a79209a65e533203508
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-09-20 08:40:13 +0600

    Create binary form of an integer.cpp

---
+++ binary form of an integer.cpp
@@ -0,0 +1,21 @@
+//Nahin the Coder
+//Date : 20-09-2021
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	bitset<8>x(a);
+	cout<<"Binary form of "<<a<<" is : "<<x;
+	//here 8 is the number of bits
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
