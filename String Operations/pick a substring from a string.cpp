ff0a6099cf01be964e0e333912fdf0cd72ab1ac5
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-10-04 14:54:18 +0600

    Create pick a substring from a string.cpp

---
+++ String Operations/pick a substring from a string.cpp
@@ -0,0 +1,17 @@
+//Coder : Nahin the Coder
+//Date : 04-10-2021
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+   cout<<"This is how we can pick a substring from a string by giving index number";
+   string s = "abcdef";
+   string p = s.substr(1,2);
+   cout<<p;
+
+   
+   return 0;
+}
